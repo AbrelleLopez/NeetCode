05e0ac690a2558e93fe27b000c17c492642ea372
Abrelle Yin Lopez <abrellelopez@gmail.com>
2026-01-25 22:25:22 +0800

    remove element - easy

---
+++ src/easy/arraysAndHashing/removeElement/Main.java
@@ -0,0 +1,12 @@
+package easy.arraysAndHashing.removeElement;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1,1,2,3,4};
+        int val = 1;
+
+        System.out.println(solution.removeElement(nums, val));
+    }
+}
